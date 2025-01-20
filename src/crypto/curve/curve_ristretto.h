38c4b9d62f35f771932df3bb9ab3ec80ecd6cfc6
elaineyu1031 <elaineyuxy@gmail.com>
2025-01-20 16:45:00 -0500

    Implement Ristretto255 curve operations
    - Add curve abstraction interface
    - Implement Ristretto255 curve using libsodium
    - Support scalar and element operations

---
+++ src/crypto/curve/curve_ristretto.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "src/crypto/curve/curve.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Implementation for curve ristretto.
+ * Example:
+ *   curve_t c;
+ *   curve_ristretto_init(&c);
+ *   unsigned char r[c.scalar_bytes];
+ *   c.scalar_random(r, c.scalar_bytes);
+ */
+
+void curve_ristretto_init(curve_t* /* curve */);
+
+#ifdef __cplusplus
+}
+#endif
